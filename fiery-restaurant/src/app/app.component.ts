284d4b51af89df6181f8e7fb59f5a93d491b184e
Saba-12 <101691122+Saba-12@users.noreply.github.com>
2022-03-20 19:48:21 +0530

    template vs template url

---
+++ fiery-restaurant/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  //templateUrl: './app.component.html',
+  template:'<h1>This is App Component</h1>',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'fiery-restaurant';
+}
